e4f2720729a78355961d383332111435fcfe7421
xingnn <xingnn@tuya.com>
2022-03-29 16:51:42 +0800

    del| 删除不用的代码

--- utils/aesutil2.py
+++ utils/aesutil2.py
@@ -13,7 +13,6 @@ pip install crypto
 pip install pycryptodome
 """
 import binascii
-import os
 import struct
 
 from Crypto.Cipher import AES
@@ -24,6 +23,35 @@ class AESCipherOld:
     def __init__(self, seckey):
         self.key = seckey  # 密钥
         self.iv = str.encode(seckey[0:16])  # 偏移量
+
+    def enc_file(self, plain_file_path, enc_file_path):
+        """
+        加密文件
+        :param plain_file_path: 明文文件路径
+        :param enc_file_path: 加密后文件路径
+        :return:
+        """
+
+        with open(plain_file_path, 'rb') as fileobj:
+            content = fileobj.read()
+            # 加密文件
+            enc_content = self.aes_encrypt(plain_data=content)
+        with open(enc_file_path, 'wb') as f:  # 以二进制写类型打开
+            f.write(enc_content)  # 写入文件
+
+    def dec_file(self, enc_file_path, dec_file_path):
+        """
+        解密文件
+        :param enc_file_path: 加密文件路径
+        :param dec_file_path: 解密文件路径
+        :return:
+        """
+        with open(enc_file_path, 'rb') as fileobj:
+            content = fileobj.read()
+            # 解密文件
+            dec_content = self.aes_decryppt(enc_data=content)
+        with open(dec_file_path, 'wb') as f:  # 以二进制写类型打开
+            f.write(dec_content)  # 写入文件
 
     def __generate_iv(self):
         iv = b'884228eb5e53a57bd0511adb60fffa8d'
@@ -55,11 +83,8 @@ class AESCipherOld:
 
         # 填充数据
         pad_data = self.__pad(text=plain_data)
-
-        encrypted_data = cipher.encrypt(pad_data)  # aes加密
-        # encrypted= base64.b64encode(cipher.encrypt(pad_data))
-        # 返回的二进制数据的十六进制表示。每一个字节的数据转换成相应的2位十六进制表示。
-        result = binascii.b2a_hex(encrypted_data)  # b2a_hex encode  将二进制转换成16进制
+        # aes加密
+        encrypted_data = cipher.encrypt(pad_data)
         return encrypted_data
 
     def aes_decryppt(self, enc_data):
@@ -71,47 +96,7 @@ class AESCipherOld:
         dec_data = cipher.decrypt(enc_data)
 
         unpad_data = self.__unpad(dec_data)
-        # 字符串转16进制
-        result = binascii.hexlify(unpad_data)
         return unpad_data
-
-    def enc_file(self, plain_file_path, enc_file_path):
-        """
-        加密文件
-        :param plain_file_path: 明文文件路径
-        :param enc_file_path: 加密后文件路径
-        :param key: 秘钥
-        :return:
-        """
-
-        with open(plain_file_path, 'rb') as fileobj:
-            content = fileobj.read()
-            # 加密文件
-            enc_content = self.aes_encrypt(plain_data=content)
-        file_name = os.path.basename(plain_file_path)
-        # out_file_path = os.path.dirname(plain_file_path) + '/enc_obama.jpg'
-
-        with open(enc_file_path, 'wb') as f:  # 以二进制写类型打开
-            f.write(enc_content)  # 写入文件
-
-    def dec_file(self, enc_file_path, dec_file_path):
-        """
-        解密文件
-        :param enc_file_path: 加密文件路径
-        :param dec_file_path: 解密文件路径
-        :param key: 秘钥
-        :return:
-        """
-        with open(enc_file_path, 'rb') as fileobj:
-            content = fileobj.read()
-            # 解密文件
-            dec_content = self.aes_decryppt(enc_data=content)
-        file_name = os.path.basename(enc_file_path)
-
-        # (filename, extension) = os.path.splitext(file_name)
-        # dec_file_path = os.path.dirname(enc_file_path) + '/dec_obama.jpg'
-        with open(dec_file_path, 'wb') as f:  # 以二进制写类型打开
-            f.write(dec_content)  # 写入文件
 
     def append_struct_info_to_encFile(self, key, plain_file_size, enc_file_path):
         """
