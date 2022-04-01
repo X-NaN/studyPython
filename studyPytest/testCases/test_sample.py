42bf98ea863206473e0405b0a851ee126433a408
xingnn <xingnn@tuya.com>
2022-04-01 14:54:13 +0800

    modify

---
+++ studyPytest/testCases/test_sample.py
@@ -0,0 +1,17 @@
+# -*- encoding: utf-8 -*-
+"""
+@File    : test_sample.py
+@Time    : 2022/3/31 6:24 下午
+@Author  : xingnana
+@Email   : 
+@Software: PyCharm
+@Description: 
+"""
+
+
+def fun(x):
+    return x + 1
+
+
+def test_answer():
+    assert fun(3) == 5
