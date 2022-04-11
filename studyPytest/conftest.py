0595378ded5102209b7b5f28560c5b507f943e60
xingnn <xingnn@tuya.com>
2022-04-11 18:10:08 +0800

    自定义命令行参数&&获取参数值

--- studyPytest/conftest.py
+++ studyPytest/conftest.py
@@ -11,6 +11,8 @@
 2、conftest.py文件名固定；
 3、conftest.py只对同一个package下的所有测试用例有效；
 4、不同目录可以有自己的conftest.py文件，一个项目可以有多个conftest.py
+5、不需要手工import conftest.py文件，pytest会自动查找；
+6、最顶层的conftest.py一般写全局的fixture
 """
 import json
 
@@ -27,19 +29,31 @@ def pytest_addoption(parser):
     :param parser:
     :return:
     """
-    print("addoption")
-    parser.addoption("--env", action="store", default=[], help="外部传入环境")
+    logger.info("pytest_addoption")
+    # action="store"时，defualt可以为任意类型值
+    parser.addoption("--env", action="store", default='', help="外部传入环境")
+    parser.addoption("--prop", action="append", default=[], help="外部传入环境")
     pass
 
 
 @pytest.fixture(scope="session")
 def cmd_configs(request):
+    logger.info("cmd_configs")
     cmd_configs = {}
     cmd_configs["env"] = request.config.getoption("--env")
 
     return cmd_configs
 
 
-@pytest.fixture(autouse=True)
+@pytest.fixture(scope="session")
+def cmd_configs1(pytestconfig):
+    logger.info("cmd_configs")
+    cmd_configs = {}
+    cmd_configs["prop"] = pytestconfig.getoption("--prop")
+
+    return cmd_configs
+
+
+@pytest.fixture()
 def configs(cmd_configs):
     logger.info('--cmd_configs的值：' + json.dumps(cmd_configs))
--- studyPytest/fixtures/test_fixture_one.py
+++ studyPytest/fixtures/test_fixture_one.py
@@ -45,5 +45,19 @@ def test_fruit_salad(fruit_bowl):
 
 
 def test_cmd_configs(cmd_configs):
-    logger.info("cmd_configs的值:" + cmd_configs["env"])
-    print("cmd_configs的值:" + cmd_configs["env"])
+    """
+    测试搜集到的命令行参数
+    :param cmd_configs: 搜集的命令行参数值
+    :return:
+    """
+    if cmd_configs["env"] == "dev":
+        logger.info("开发环境，cmd_configs的值:" + str(cmd_configs["env"]))
+    elif cmd_configs["env"] == "daily":
+        logger.info("日常环境，cmd_configs的值:" + str(cmd_configs["env"]))
+
+    # logger.info("cmd_configs的值:" + str(cmd_configs1["prop"]))
+
+
+if __name__ == '__main__':
+    # pytest.main(["-s", "./test_fixture_one.py/", "--env=test"])
+    pytest.main(["-s", "./test_fixture_one.py/", "--env=dev", "--prop=nana", "--prop=leo"])
