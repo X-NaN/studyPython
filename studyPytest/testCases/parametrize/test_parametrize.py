fa28b44291eef15dd3623e06090782f66b23f4b3
xingnn <xingnn@tuya.com>
2022-07-08 11:24:04 +0800

    feat: parametrize

--- studyPytest/testCases/parametrize/test_parametrize.py
+++ studyPytest/testCases/parametrize/test_parametrize.py
@@ -12,6 +12,14 @@ import pytest
 
 @pytest.mark.parametrize("test_input,expected", [("3+5", 8), ("2+4", 6), ("6*9", 42)])
 def test_eval(test_input, expected):
+    """
+    生成三个用例
+    test_eval[3+5-8]
+    test_eval[2+4-6]
+    :param test_input:
+    :param expected:
+    :return:
+    """
     assert eval(test_input) == expected
 
 
@@ -20,6 +28,9 @@ def test_eval(test_input, expected):
 def test_multi(x, y):
     """
     生成四个用例，执行四次。3-0，3-1，4-0，，4-1
+    test_multi[3-0]
+    test_multi[3-0]
+    ...
     :param x:
     :param y:
     :return:
